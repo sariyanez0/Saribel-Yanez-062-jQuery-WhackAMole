e256c156700fba4c097554d29e68e982e35f953e
sariyanez0 <144826616+sariyanez0@users.noreply.github.com>
2023-12-19 11:36:11 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,45 @@
+$(".mole1").click(function(){
+    $(".mole1").hide();
+    $(".third-mole").show();
+});
+    
+$(".mole-two").click(function(){
+    $(".mole-two").hide();
+    $(".6-mole").show();
+});
+    
+$(".third-mole").click(function(){
+    $(".third-mole").hide();
+});
+   
+
+$(".fourth-mole").click(function(){
+    $(".fourth-mole").hide();
+});
+$(".5-mole").click(function(){
+    $(".5-mole").hide();
+    $(".7-mole").show();
+});
+$(".6-mole").click(function(){
+    $(".6-mole").hide();
+    $(".8-mole").show();
+});
+$(".7-mole").click(function(){
+    $(".7-mole").toggle();
+});
+
+$(".8-mole").click(function(){
+    $(".8-mole").hide();
+    $(".fourth-mole").show();
+    $(".5-mole").show();
+});
+
+
+$(".mole-9").click(function(){
+    $(".mole-9").hide();
+});
+
+$(".reset").click(function(){
+    $("img").show();
+});
+
